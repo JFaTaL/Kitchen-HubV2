09b9e4711546c241b737b83d1900003ffc70935f
james <jamesjulian974138@gmail.com>
2024-04-06 14:58:31 -0400

    refactor - clean up kitchenInventory.java to to cleaner

--- src/main/kitchenInventory.java
+++ src/main/kitchenInventory.java
@@ -13,49 +13,35 @@ public class kitchenInventory {
         kitchenList.put(itemName, new ItemDetails(itemQuantity, expirationDate));
     }
 
-    public static void viewKitchen(){
+    public static void viewKitchen() {
         System.out.println("Kitchen List");
         System.out.println("----------------");
-        for(Map.Entry<String, ItemDetails> entry : getKitchenList().entrySet() ){
+        for(Map.Entry<String, ItemDetails> entry : getKitchenList().entrySet()) {
             String itemName = entry.getKey();
             ItemDetails itemDetails = entry.getValue();
-            System.out.println("Item: " + itemName);
-            System.out.println("Quantity: " + itemDetails.getQuantity());
-            if(itemDetails.getExpirationDate() == null){
-                System.out.println("Expiration Date: N/A");
-                break;
-            }
-            System.out.println("Expiration Date: " + itemDetails.getExpirationDate());
-            System.out.println("---------------------------");
+            printItemDetails(itemName, itemDetails);
         }
     }
 
-    public static void addExpiration(){
+    public static void addExpiration() {
         Scanner scnr = new Scanner(System.in);
-        System.out.println("Kitchen List");
-        System.out.println("----------------");
-        for(Map.Entry<String, ItemDetails> entry : getKitchenList().entrySet() ){
+        for(Map.Entry<String, ItemDetails> entry : getKitchenList().entrySet()) {
             String itemName = entry.getKey();
             ItemDetails itemDetails = entry.getValue();
-            System.out.println("Item: " + itemName);
-            System.out.println("Quantity: " + itemDetails.getQuantity());
-            System.out.println("---------------------------");
-            System.out.println("Add Expiration Date? (Y/N)");
+            printItemDetails(itemName, itemDetails);
+            System.out.println("Add Expiration Date for " + itemName + "? (Y/N)");
             String userChoice = scnr.nextLine().toLowerCase();
-            if(!userChoice.equals("y")){
-                break;
+            if(userChoice.equals("y")) {
+                System.out.println("Enter Expiration Date (MM-DD-YYYY) ");
+                String expirationDate = scnr.nextLine();
+                itemDetails.setExpirationDate(expirationDate);
             }
-            
-            System.out.println("Enter Expiration Date (MM-DD-YYYY) ");
-            String expirationDate = scnr.nextLine();
-            itemDetails.setExpirationDate(expirationDate);
-
         }
         main.kitchenMenu();
     }
 
     public static void editKitchenItem(){
-        /* Scanner scnr = new Scanner(System.in);
+        /*Scanner scnr = new Scanner(System.in);
         viewKitchen();
         System.out.println("Please enter name of the item you wish to edit");
         System.out.println("-----------------");
@@ -83,7 +69,7 @@ public class kitchenInventory {
                 System.out.println("Enter new Quantity");
                 int newQuantity = scnr.nextInt();
                 scnr.nextLine();
-                getKitchenList().put(itemName, newQuantity);
+                getKitchenList().put(itemName, itemDetails.setq);
                 break;
             
             case 3:
@@ -91,6 +77,17 @@ public class kitchenInventory {
         }
         main.mainMenu(); */
         
+    }
+
+    public static void printItemDetails(String itemName, ItemDetails itemDetails) {
+        System.out.println("Item: " + itemName);
+        System.out.println("Quantity: " + itemDetails.getQuantity());
+        if(itemDetails.getExpirationDate() == null) {
+            System.out.println("Expiration Date: N/A");
+        } else {
+            System.out.println("Expiration Date: " + itemDetails.getExpirationDate());
+        }
+        System.out.println("---------------------------");
     }
     
     public static Map<String, ItemDetails> getKitchenList() {
